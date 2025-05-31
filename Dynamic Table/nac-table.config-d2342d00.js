c57cee820c523cbf6db78734cec81d35fdbf0cc3
Zulfikar <zulfikar97@gmail.com>
2025-05-31 19:47:12 +0700

    Add metadata config

--- Dynamic Table/nac-table.config-d2342d00.js
+++ Dynamic Table/nac-table.config-d2342d00.js
@@ -1,25 +1,28 @@
 const config = {
-  controlName: 'NAC Table',
-  description: 'Editable Table for NAC',
-  groupName: 'Elistec Form Controls',
+  controlName: "Editable Table",
+  description: "Editable Table for NAC",
+  groupName: "Elistec Form Controls",
   fallbackDisableSubmit: false,
+  iconUrl: "repeating-section",
+  searchTerms: ["list", "table", "repeat", "section", "repeating section", "elistec", "eiu"],
+  pluginAuthor: "Nintex, Zulfikar",
   standardProperties: {
     readOnly: true,
     required: false,
-    description: true
+    description: true,
   },
-  version: '1.0',
+  version: "1.0",
   properties: {
     collection: {
-      type: 'string',
-      title: 'JSON Data to be surfaced or displayed'
+      type: "string",
+      title: "JSON Source",
     },
     value: {
-      type: 'string',
-      title: 'Updated JSON',
-      isValueField: true
-    }
-  }
+      type: "string",
+      title: "Updated JSON",
+      isValueField: true,
+    },
+  },
 };
 
 export { config };
