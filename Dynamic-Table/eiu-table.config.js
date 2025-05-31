66e5269a2f80885f3755b80a386ebb5242a0d3b3
Zulfikar <zulfikar97@gmail.com>
2025-06-01 01:15:16 +0700

    more revision on searchable

--- Dynamic-Table/eiu-table.config.js
+++ Dynamic-Table/eiu-table.config.js
@@ -30,6 +30,7 @@ const config = {
     searchable: {
       type: "boolean",
       title: "Searchable",
+      defaultValue: true,
     },
   },
 };
--- Dynamic-Table/eiu-table.js
+++ Dynamic-Table/eiu-table.js
@@ -411,7 +411,6 @@ let AndysTable = _decorate(
     class AndysTable extends _LitElement {
       constructor(...args) {
         super(...args);
-        this.searchable = true;
         _initialize(this);
       }
     }
