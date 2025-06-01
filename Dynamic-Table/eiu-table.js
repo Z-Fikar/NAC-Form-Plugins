e9feccaacb7a9989d6aebb639d3ca7997352ce7c
Zulfikar <zulfikar97@gmail.com>
2025-06-01 22:38:06 +0700

    move config

--- Dynamic-Table/eiu-table.js
+++ Dynamic-Table/eiu-table.js
@@ -1,5 +1,67 @@
 import { i, _ as _decorate, s, e, y, a as e$1 } from "./query-assigned-elements.js";
 
+const config = {
+  controlName: "Editable Table",
+  description: "Editable Table for NAC",
+  groupName: "Elistec Form Controls",
+  fallbackDisableSubmit: true,
+  iconUrl: "repeating-section",
+  searchTerms: ["list", "table", "repeat", "section", "repeating section", "elistec", "eiu"],
+  pluginAuthor: "Nintex, Zulfikar",
+  standardProperties: {
+    readOnly: true,
+    required: false,
+    description: true,
+    visibility: true,
+  },
+  version: "1.0",
+  properties: {
+    collection: {
+      type: "string",
+      title: "JSON Source",
+    },
+    value: {
+      type: "string",
+      title: "Updated JSON",
+      isValueField: true,
+    },
+    pageSize: {
+      type: "integer",
+      title: "Page Size",
+    },
+    searchable: {
+      type: "boolean",
+      title: "Searchable",
+      defaultValue: true,
+    },
+    rowNumber: {
+      type: "boolean",
+      title: "Row Number",
+    },
+    lookupLabels: {
+      type: "string",
+      title: "Lookup Labels",
+      defaultValue: JSON.stringify([
+        ["satu", "dua", "tiga"],
+        ["empat", "lima", "enam"],
+      ]),
+    },
+    lookupValues: {
+      type: "string",
+      title: "Lookup Values",
+      defaultValue: JSON.stringify([
+        [1, 2, 3],
+        [4, 5, 6],
+      ]),
+    },
+    lookupMapping: {
+      type: "string",
+      title: "Lookup Mapping",
+      defaultValue: JSON.stringify(["Event", "Options"]),
+    },
+  },
+};
+
 const baseStyle = i`
   :host {
     height: 100%;
@@ -668,7 +730,7 @@ let AndysTable = _decorate([e$1("eiu-table")], function (_initialize, _LitElemen
       static: true,
       key: "getMetaConfig",
       value: function getMetaConfig() {
-        return import("./eiu-table.config.js").then((x) => x.config);
+        return this.config;
       },
     },
   ];
